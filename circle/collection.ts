eb7675eb84c4b28e417ddde17bd4f60aa5b8b819
Haley Nakamura <halnak@mit.edu>
2022-10-17 21:23:43 -0400

    Implemented Circle model and collection

---
+++ circle/collection.ts
@@ -0,0 +1,73 @@
+import type {HydratedDocument, Types} from 'mongoose';
+import type {Circle} from './model';
+import CircleModel from './model';
+import UserModel from 'user/model';
+
+/**
+ * This file contains a class with functionality to interact with circle stored
+ * in MongoDB, including adding, finding, updating, and deleting. Feel free to add
+ * additional operations in this file.
+ *
+ * Note: HydratedDocument<Circle> is the output of the CircleModel() constructor,
+ * and contains all the information in User. https://mongoosejs.com/docs/typescript.html
+ */
+class CircleCollection {
+  /**
+   * Add a new Circle
+   *
+   * @param {string} name - The name of the circle
+   * @param {{ type: Types.ObjectId, ref: 'User' }} owner - The username of the owner
+   * @return {Promise<HydratedDocument<Circle>>} - The newly created user
+   */
+  static async addOne(name: string, owner: {type: Types.ObjectId, ref: 'User'}): Promise<HydratedDocument<Circle>> {
+    const members = [owner]; // owner is first member of circle
+    const freets: Array<{ type: Types.ObjectId, ref: 'Freet' }> = [];
+    const circle = new CircleModel({name, owner, members, freets});
+    await circle.save(); // Saves user to MongoDB
+    return circle;
+  }
+
+  /**
+   * Find a circle by circleId.
+   *
+   * @param {string} circleId - The circleId of the circle to find
+   * @return {Promise<HydratedDocument<Circle>> | Promise<null>} - The circle with the given circleId, if any
+   */
+  static async findOneByCircleId(circleId: Types.ObjectId): Promise<HydratedDocument<Circle>> {
+    return CircleModel.findOne({_id: circleId});
+  }
+
+  /**
+   * Update members of a circle. 
+   *
+   * @param {string} circleId - The circleId of the circle to update
+   * @param {string} userId - The userId of the user to add or remove from circle members
+   * @param {boolean} addUser - Whether to add (true) or remove (false) the user from the circle members
+   * @return {Promise<HydratedDocument<Circle>>} - The updated circle
+   */
+  static async updateOne(circleId: Types.ObjectId, userId: Types.ObjectId | string, addUser: boolean): Promise<HydratedDocument<Circle>> {
+    const circle = await CircleModel.findOne({_id: circleId});
+    const user = await UserModel.findOne({_id: userId});
+    const index = circle.members.indexOf(user.id, 0);
+    if (addUser && (index > -1)){
+      circle.members.push(user.id)
+    }else if (!addUser && (index === -1)){
+      circle.members.splice(index, 1);
+    }
+    await circle.save();
+    return circle;
+  }
+
+  /**
+   * Delete a circle from the collection.
+   *
+   * @param {string} circleId - The circleId of user to delete
+   * @return {Promise<Boolean>} - true if the circle has been deleted, false otherwise
+   */
+  static async deleteOne(circleId: Types.ObjectId): Promise<boolean> {
+    const circle = await CircleModel.deleteOne({_id: circleId});
+    return circle !== null;
+  }
+}
+
+export default CircleCollection;
---
+++ circle/model.ts
@@ -0,0 +1,47 @@
+import type {Types} from 'mongoose';
+import {Schema, model} from 'mongoose';
+
+/**
+ * This file defines the properties stored in a Circle
+ * DO NOT implement operations here ---> use collection file
+ */
+
+// Type definition for User on the backend
+export type Circle = {
+  _id: Types.ObjectId; // MongoDB assigns each object this ID on creation
+  name: string; 
+  owner: string; 
+  members: [{ type: Types.ObjectId, ref: 'User' }];
+  freets: [{ type: Types.ObjectId, ref: 'Freet' }];
+};
+
+// Mongoose schema definition for interfacing with a MongoDB table
+// Users stored in this table will have these fields, with the
+// type given by the type property, inside MongoDB
+const CircleSchema = new Schema({
+  // The name of the circle
+  name: { 
+    type: String,
+    required: true
+  },
+  // Username of the owner
+  owner: {
+    type: String,
+    required: true
+  },
+  // Members of the circle
+  members: {
+    type: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    required: true,
+    ref: 'User'
+  },
+  // Freets posted to this circle
+  freets: {
+    type: [{ type: Schema.Types.ObjectId, ref: 'Freet' }],
+    required: false,
+    ref: 'Freet'
+  }
+});
+
+const CircleModel = model<Circle>('Circle', CircleSchema);
+export default CircleModel;
